6f391de1007bbd84c3036a3406e4452e6a231d7f
bbedevian <brettbedevian@gmail.com>
2020-05-22 13:50:27 -0400

    login slash signup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import './App.css';
+import LoginSignup from './Components/LoginSignup';
 
 class App extends React.Component {
 
   state = {
+    users: [],
     currentUserId: null,
     userItems: [],
     userGundams: [],
+    userBalance: []
   }
+
+
+  componentDidMount() {
+      fetch(`http://localhost:3000/users`)
+      .then(resp => resp.json())
+      .then(users => this.setState({ users}))
+  }
+
+  setCurrentUser = (user) => {
+    this.setState({currentUserId: user})
+  }
+
   render() {
+    console.log('App State :>> ', this.state);
+    const {setCurrentUser} = this
+    const {users, currentUserId} = this.state
     return (
       <div>
+        { currentUserId ? null : 
+        <LoginSignup setCurrentUser={setCurrentUser} users={users} />
+      }
         {/* ternary based on currentuserId cant be null to show below, else show <login/signup/> */}
         {/* {<NavBar/>}
         below is the switch based on whats clicked(starts on profile) */}
