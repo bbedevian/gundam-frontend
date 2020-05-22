888c638fd637b91c61334a3765327f302609e9d8
bbedevian <brettbedevian@gmail.com>
2020-05-22 10:35:43 -0400

    more setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-  
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    currentUserId: null,
+    userItems: [],
+    userGundams: [],
+  }
+  render() {
+    return (
+      <div>
+        {/* ternary based on currentuserId cant be null to show below, else show <login/signup/> */}
+        {/* {<NavBar/>}
+        below is the switch based on whats clicked(starts on profile) */}
+      </div>
+    );
+  }
 }
 
 export default App;
