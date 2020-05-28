16694f7842ac14600c3e6468492ec2f54416601d
Josephscha <Josephshincha@gmail.com>
2020-05-27 21:53:45 -0400

    got rid of annoying integer return from settimeout

--- src/Components/EnemyGundam.js
+++ src/Components/EnemyGundam.js
@@ -3,29 +3,25 @@ import { Link } from 'react-router-dom';
 
 class EnemyGundam extends Component {
 
-
+    
     render() {
         const {opponents, myTurn, getAttacked, opponentHealth, currentUser, selectedLevel} = this.props
         console.log('enemy gundam props :>> ', this.props);
         console.log('current user :>> ', currentUser);
         // let attack = Math.floor(Math.random() * (500 - 200 + 1)) + 25
         let currentOpponent = opponents[selectedLevel-1]
-
+        if (opponentHealth > 0) {
+            if (!myTurn) {
+                setTimeout(() => getAttacked(currentOpponent.attack), 3500)
+            }
+        }
         return (
             <div className="split left">
                 <>
                 <h1 >{currentOpponent.name}</h1>
                 <img className="image200" src={currentOpponent.img_url} alt="nothing to see here"/>
                 <p >Enemy Health: {opponentHealth}</p>
-                </>
-
-                {opponentHealth >0 ? 
-                myTurn ? null : setTimeout(() => getAttacked(currentOpponent.attack), 3500)
-                :
-                null
-                 }
-                
-                
+                </>     
             </div>
         );
     }
